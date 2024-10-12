0cdf17f32458906480f0418385b2d643030f5ea9
majidaniel <danielg60@hotmail.com>
2024-10-12 10:45:54 -0400

    Started adding controller support

--- resources/InputCapture.hx
+++ resources/InputCapture.hx
@@ -1,11 +1,13 @@
 package resources;
 
+import hxd.Pad;
 import Types.GameAction;
 
 // Singleton that hooks into Heaps.io's keyboard even system and translates it into action enum
 class InputCapture {
 	var keycodeMap:Map<Int, GameAction> = new Map();
 	var actionStatus:Map<GameAction, Bool> = new Map();
+	var pad:Pad;
 
 	public function new() {
 		hxd.Window.getInstance().addEventTarget(onEvent);
@@ -14,7 +16,20 @@ class InputCapture {
 		keycodeMap.set(40, GameAction.MoveDown);
 		keycodeMap.set(37, GameAction.MoveLeft);
 		keycodeMap.set(32, GameAction.MergeAction);
+
+		hxd.Pad.wait(onPad);
 	}
+
+	function onPad( p : hxd.Pad ){
+		if( !p.connected )
+		  trace("Pad not connected ?");
+		p.onDisconnect = function(){
+		  if( p.connected )
+			trace("OnDisconnect called while still connected ?");
+		}
+		this.pad=p;
+	  }
+
 
 	public function onEvent(event:hxd.Event) {
 		switch (event.kind) {
@@ -31,6 +46,7 @@ class InputCapture {
 	}
 
 	public function getActionStatus(gameAction:GameAction) {
+		trace(pad);
 		var status = actionStatus.get(gameAction);
 		if (status == null)
 			return false;
